b3671e823a6e2a9fb2eeb3fdbac619b04a0ac632
Dhillon <dhillon.thu@gmail.com>
2025-02-03 15:45:21 +0000

    Fixed Reed webscrape

--- job-searcher-app/src/Server/Server.py
+++ job-searcher-app/src/Server/Server.py
@@ -8,8 +8,7 @@ CORS(app)
 
 @app.route('/scrape', methods=['GET'])
 def scrape():
-    url = 'https://www.reed.co.uk/jobs/software-jobs-in-mitcham'
-
+    data = []
     headers = {
     "User-Agent": (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
@@ -21,23 +20,26 @@ def scrape():
     "Accept-Encoding": "gzip, deflate, br",
     "Connection": "keep-alive",
     }
+
+    #####REED#####
+
+    url = 'https://www.reed.co.uk/jobs/software-jobs-in-mitcham'
     response = requests.get(url, headers=headers)
-    data = []
 
     if response.status_code != 200:
-        print(response.status_code)
         return jsonify({"error": "Failed to load page"}), 500
     soup = BeautifulSoup(response.text, 'html.parser')
     jobs = soup.find_all('article', attrs={'class': 'card job-card_jobCard__MkcJD'})
 
     is_Next = True
+    i = 1
 
-    while is_Next == True:
+    while is_Next == True and i < 0:
         next_button = soup.find('a', attrs={'class': 'page-link next'}).parent
         if next_button.get('class') == ['page-item disabled']:
-            is_Next = True
-        else:
             is_Next = False
+        else:
+            is_Next = True
 
         for job in jobs:
             div_element = job.find('a', attrs={'class': 'job-card_jobTitle__HORxw'})
@@ -50,26 +52,49 @@ def scrape():
                     'description': description,
                     'link': f'{link}'
                 })
+        i += 1
+        url = f'https://www.reed.co.uk/jobs/software-jobs-in-mitcham?pageno={i}'
+        response = requests.get(url, headers=headers)
+
+        if response.status_code != 200:
+            print(response.status_code)
+            return jsonify({"error": "Failed to load page"}), 500
+        soup = BeautifulSoup(response.text, 'html.parser')
+        jobs = soup.find_all('article', attrs={'class': 'card job-card_jobCard__MkcJD'})
+
+
+#####JOBSITE#####
 
     url = 'https://www.jobsite.co.uk/jobs/software/in-london?radius=20'
     response = requests.get(url, headers=headers)
 
     if response.status_code != 200:
+        print(response.status_code)
         return jsonify({"error": "Failed to load page"}), 500
     soup = BeautifulSoup(response.text, 'html.parser')
-    jobs = soup.find_all('li', attrs={'class': 'css-1ac2h1w eu4oa1w0'})
+    jobs = soup.find_all('article', attrs={'class': 'res-817i67'})
 
-    for job in jobs:
-        div_element = job.find('div', attrs={'aria-live': 'polite'})
-        if div_element is not None:
-            title = job.find('span', attrs={'title': True}).text
-            description = job.find('span', attrs={'data-testid': 'company-name'}).text
-            link = job.find('a', attrs={'class': 'jcs-JobTitle css-1baag51 eu4oa1w0'}).get('href')
-            data.append({
-                'title': title,
-                'description': description,
-                'link': f'{link}'
-            })
+    is_Next = True
+    i = 1
+
+    while is_Next == True and i < 0:
+        #Change next button direction
+        next_button = soup.find('a', attrs={'class': 'page-link next'}).parent
+        if next_button.get('class') == ['page-item disabled']:
+            is_Next = False
+        else:
+            is_Next = True
+        
+        for job in jobs:
+            if job is not None:
+                title = job.find('div', attrs={'class': 'res-nehv70'}).text
+                description = job.find('span', attrs={'class': 'res-c0o836'}).text
+                link = job.find('a', attrs={'class': 'res-1w4glxj'}).get('href')
+                data.append({
+                    'title': title,
+                    'description': description,
+                    'link': f'{link}'
+                })
 
     return jsonify(data)
 
